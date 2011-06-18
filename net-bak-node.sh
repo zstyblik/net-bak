2612cb8ad8a7b02997142684786a7a815e6dbfb4
Zdenek Styblik <stybla@turnovfree.net>
2011-06-18 13:30:16 +0200

    MOD simplified script for node part Signed-off-by: Zdenek Styblik <stybla@turnovfree.net>
    Signed-off-by: Zdeněk Styblík <stybla@turnovfree.net>

--- net-bak-node.sh
+++ net-bak-node.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
-#
+# Desc:
+# ---
+# This script is being called from back-up server via ssh
+# Place this at node which should be backed-up along with file 
+# '2backup.txt' which is full-path list of files/dirs to back-up
+# ---
 # 2009/Sep/05 @ Zdenek Styblik
 #
 # Simple back-up script for backing-up from misc openwrt devices
 # all around the network
 #
-# This script is being called from back-up server via ssh
-# Place this at node which should be backed-up along with file 
-# '2backup.txt' which is full-path list of files/dirs to back-up
-#
 # License? What?! Who would have use this crap anyway?! :)
 #
 
-LIST=''
-for item in `cat 2backup.txt`; do
-	LIST="${LIST} ${item}";
-done
+FILESTOBAK="files2backup.txt"
 
-tar czf - ${LIST} 2>/dev/null
+if [ ! -e "${FILESTOBAK}" ]; then
+	exit 100
+fi
+
+cat "${FILESTOBAK}" | tr '\n' ' ' | xargs tar czf - 2>/dev/null
 
 exit 0
 
